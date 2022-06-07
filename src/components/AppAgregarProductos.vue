6670b439517923d8b992d0af45e4aae0de025632
martín etcheberry <msetcheberry@gmail.com>
2022-06-06 23:33:10 -0300

    Agrego el inicio del componente Producto y mejoro agregar prod

--- src/components/AppAgregarProductos.vue
+++ src/components/AppAgregarProductos.vue
@@ -34,34 +34,59 @@
         </div>
       </div>
     </div>
-    <div class="">
-      <h1 class="">Prods favs</h1>
+    <div class="row mx-0">
+      <h1 class="text-center mt-5">Productos Favoritos</h1>
+      <div
+        class="col-sm-4 mt-4"
+        v-for="product in productosFav"
+        :key="product.id"
+      >
+        <AppProducto :product="product"></AppProducto>
+      </div>
     </div>
-    <div class="">
-      <h3 class="">Ofertas</h3>
+
+    <div class="row mt-3">
+      <h1 class="text-center mt-5">Ofertas</h1>
+      <div class="col-sm-4 mt-4" v-for="product in ofertas" :key="product.id">
+        <AppProducto :product="product"></AppProducto>
+      </div>
     </div>
-    <div class="">
-      <h1 class="">lista ofertas</h1>
-    </div>
-    <div class="">
-      <h3 class="">Stock y precios</h3>
-      <h1 class="">Stock general</h1>
+
+    <div class="row mx-0">
+      <h1 class="text-center mt-5">Stock general</h1>
+      <div
+        class="col-sm-4 mt-4"
+        v-for="product in stockGeneral"
+        :key="product.id"
+      >
+        <AppProducto :product="product"></AppProducto>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import AppProducto from "@/components/AppProducto.vue";
 export default {
-  components: { 
+  components: {
+    AppProducto,
   },
  
   data(){
     return{
-    productos: [
+    prod:{ id: 1,
+    name: "Oreos pack x6",
+    brand:"Arcor",
+    price: 500.5,
+    category:"Galletitas",
+    amount: 3,},
+
+    productosFav: [
   {
     id: 1,
     name: "Oreos",
     brand:"arcor",
+    category:"Galletitas",
     price: 120.5,
     amount: 3,
   },
@@ -69,15 +94,120 @@ export default {
     id: 2,
     name: "Chocolinas",
     brand:"arcor",
+    category:"Galletitas",
     price: 100.99,
     amount: 3,
   },
   {
-    id: 3,
-    name: "Pepitos",
+    id: 4,
+    name: "Merengadas",
     brand:"arcor",
+    category:"Galletitas",
     price: 115.0,
     amount: 3,
+  },
+   {
+    id: 5,
+    name: "Rumba",
+    brand:"arcor",
+    category:"Galletitas",
+    price: 115.0,
+    amount: 3,
+  },
+  ],
+     ofertas: [
+  {
+    id: 1,
+    name: "Aceite 1,5Lt",
+    brand:"Marolio",
+    category:"Alacena",
+    price: 200,
+  },
+   {
+    id: 2,
+    name: "Vino Malbec 2,25Lt",
+    brand:"Elvi nito",
+    category:"Bebidas",
+    price: 235,
+  },
+   {
+    id: 3,
+    name: "Arroz 3x2 1 kg",
+    brand:"Lucchetti",
+    category:"Alacena",
+    price: 360,
+  },
+   {
+    id: 4,
+    name: "Aceite 1,5Lt",
+    brand:"Marolio",
+    category:"Alacena",
+    price: 200,
+  },
+   {
+    id: 5,
+    name: "Puré de tomate 520Gr",
+    brand:"Día",
+    category:"Alacena",
+    price: 59,
+  },
+  ],
+    stockGeneral: [
+  {
+    id: 1,
+    name: "Servilletas 70U",
+    brand:"Día",
+    category:"Higiene y limpieza",
+    price: 126,
+  },
+   {
+    id: 2,
+    name: "Enjuage bucal Minty fresh(? 250Ml",
+    brand:"Colgate",
+    category:"Higiene y limpieza",
+    price: 400.33,
+   },
+   {
+    id: 3,
+    name: "Cerveza Lager 500Ml x6",
+    brand:"Starberg",
+    category:"Bebidas",
+    price: 1100,
+  },
+   {
+    id: 4,
+    name: "Salame Fetado Lario 150Gr",
+    brand:"Milán",
+    category:"Heladera",
+    price: 200,
+  },
+   {
+    id: 5,
+    name: "Nuez pelada en cubeta 100gr",
+    brand:"La Sanjuanita",
+    category:"Dietética",
+    price: 239.25
+  },
+  {
+    id: 6,
+    name: "Durazno en mitades 820Gr",
+    brand:"Coto",
+    category:"Almacen",
+    price: 355
+  },
+  {
+    id: 7,
+    name: "Milanesa de soja calabaza x4",
+    brand:"Granja del Sol",
+    category:"Congelados",
+    price: 359
+  },
+  {
+    id: 8,
+    name: "Palta x 1Un",
+    brand:"Carrefour",
+    category:"Frutas y verduras",
+    price: 79.00,
   },
   ]
   }}
---
+++ src/components/AppProducto.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="card text-center" style="width: 25rem">
+      <div class="card-body">
+        <h5 class="card-title">{{ product.name }}</h5>
+        <div class="row">
+          <h6 class="col-sm">Categoria: {{ product.category }}</h6>
+          <h6 class="col-sm">Marca: {{ product.brand }}</h6>
+          <h6 class="">Precio: ${{ product.price }}</h6>
+        </div>
+        <p class="card-text"></p>
+        <a href="#" class="btn boton">Agregar a la lista</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"AppProducto",
+    props: {
+        product: Object,
+    },
+    data() {
+        return {
+        
+        };
+    },
+}
+</script>
+
+<style>
+.boton {
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #000000;
+  text-align: center;
+  text-decoration: none;
+  vertical-align: middle;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  background-color: transparent;
+  border: 5px solid #ede3fc;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+</style>
