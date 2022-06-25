a951622b84daa3c2bcf5fc8725c3375a272504e6
CandeKrummer <43725125@comunidadort.edu.ar>
2022-06-25 20:45:22 -0300

    Corregido un pequeño error en el router

--- src/router.js
+++ src/router.js
@@ -48,7 +48,7 @@ const routes = [
         component: AppAgregarProductos,
     },
     {
-        path: '/realizar-compra"',
+        path: '/realizar-compra',
         name: 'realizarCompra',
         component: AppRealizarCompra,
     },
